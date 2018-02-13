339e0794be9a69906b64e48f51f61a4c9d6a8176
Weiran Xiong <weiranxiong@gmail.com>
2018-02-12 23:06:11 -0800

    test

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react
+import './404.css'
+
+class 404 extends Component {
+    render() {
+        return (
+
+        )
+    }
+}
+
+export default 404
--- src/components/QuizReview/QuizReview.js
+++ src/components/QuizReview/QuizReview.js
@@ -5,15 +5,19 @@ import './Quiz2.css'
 import ReactMarkdown from 'react-markdown'
 import axios from 'axios'
 
-class Quiz2 extends Component {
+class Quiz extends Component {
     constructor() {
         super()
         this.state = {
             quizMarkdownData: "",
-            dataUrl: "https://raw.githubusercontent.com/weiranx/cse12_quiz2/master/quiz2.md"
+            quizNum: 1,
+            dataUrl: ""
         }
     }
     componentDidMount() {
+        this.setState({
+            dataUrl: "https://raw.githubusercontent.com/weiranx/cse12_quiz2/master/quiz"+this.state.quizNum+".md"
+        })
         axios.get(this.state.dataUrl).then(res => {
             this.setState({
                 quizMarkdownData: res.data
